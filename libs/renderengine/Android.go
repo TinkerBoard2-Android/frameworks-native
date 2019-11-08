01d43d5df911ad7dcf1330fccc45bd5bedd2304b
Zhixiong Lin <zhixiong.lin@rock-chips.com>
2019-11-08 11:25:33 +0800

    SurfaceFlinger: Neon transform for NV12_10.
        1.Neon transform NV12_10 to NV12 for 3288
        2.Neon transform usage ARM_P010 for 3399
        3.Delete Android.go print
    
    Change-Id: I04814b99a7433ede5f6960f82f13e5a206831870
    Signed-off-by: Zhixiong Lin <zhixiong.lin@rock-chips.com>

--- libs/renderengine/Android.go
+++ libs/renderengine/Android.go
@@ -3,13 +3,14 @@ package librenderengine
 import (
         "android/soong/android"
         "android/soong/cc"
-        "fmt"
+//        "fmt"
         "strings"
 )
 
 func init() {
 	//该打印会在执行mm命令时，打印在屏幕上
-	fmt.Println("librenderengine want to conditional Compile")
+    //fmt.Println("librenderengine want to conditional Compile")
+
 	android.RegisterModuleType("cc_librenderengine", DefaultsFactory)
 }
 
@@ -31,7 +32,7 @@ func Defaults(ctx android.LoadHookContext) {
 func globalDefaults(ctx android.BaseContext) ([]string) {
 	var cppflags []string
 	//该打印输出为: TARGET_PRODUCT:rk3328 fmt.Println("TARGET_PRODUCT:",ctx.AConfig().Getenv("TARGET_PRODUCT")) //通过 strings.EqualFold 比较字符串，可参考go语言字符串对比
-    fmt.Println("rk-debug-TARGET_PRODUCT:",ctx.AConfig().Getenv("TARGET_BOARD_PLATFORM_GPU")) //通过 strings.EqualFold 比较字符串，可参考go语言字符串对比
+    //fmt.Println("TARGET_BOARD_PLATFORM_GPU:",ctx.AConfig().Getenv("TARGET_BOARD_PLATFORM_GPU")) //通过 strings.EqualFold 比较字符串，可参考go语言字符串对比
     if (strings.EqualFold(ctx.AConfig().Getenv("TARGET_BOARD_PLATFORM_GPU"),"mali-t720")) {
 	//添加 DEBUG 宏定义
         cppflags = append(cppflags,"-DMALI_PRODUCT_ID_T72X=1")
@@ -48,6 +49,21 @@ func globalDefaults(ctx android.BaseContext) ([]string) {
     } else if (strings.EqualFold(ctx.AConfig().Getenv("TARGET_BOARD_PLATFORM_GPU"),"mali450")) {
         cppflags = append(cppflags,"-DMALI_PRODUCT_ID_450=1")
     }
+
+    //fmt.Println("TARGET_PRODUCT:",ctx.AConfig().Getenv("TARGET_PRODUCT"))
+    if (strings.Contains(ctx.AConfig().Getenv("TARGET_PRODUCT"),"rk3288")) {
+        cppflags = append(cppflags,
+           "-DRK_NV12_10_TO_NV12_BY_RGA=0",
+           "-DRK_NV12_10_TO_NV12_BY_NENO=1")
+    }else if (strings.Contains(ctx.AConfig().Getenv("TARGET_PRODUCT"),"rk3399")){
+        cppflags = append(cppflags,
+            "-DRK_HDR=1")
+    }else{
+        cppflags = append(cppflags,
+            "-DRK_NV12_10_TO_NV12_BY_RGA=1",
+            "-DRK_NV12_10_TO_NV12_BY_NENO=0")
+    }
+
 	//将需要区分的环境变量在此区域添加 //....
 	return cppflags
 }
